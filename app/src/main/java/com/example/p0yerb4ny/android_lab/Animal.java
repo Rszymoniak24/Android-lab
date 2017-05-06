b2f05a61d504a3f5295d477ad501c7ca95eb2b25
Rszymoniak <djxr24@gmail.com>
2017-05-06 12:12:58 +0200

    first commit

---
+++ app/src/main/java/com/example/p0yerb4ny/android_lab/Animal.java
@@ -0,0 +1,40 @@
+package com.example.p0yerb4ny.android_lab;
+
+import java.io.Serializable;
+
+/**
+ * Created by P0YERB4NY on 06.05.2017.
+ */
+
+public class Animal implements Serializable {
+
+    private int id;
+    private String gatunek;
+    private String kolor;
+    private float wielkosc;
+    private String opis;
+
+    public Animal(){}
+
+    public Animal (String gatunek,String kolor,float wielkosc, String opis){
+        super();
+        this.gatunek=gatunek;
+        this.kolor=kolor;
+        this.wielkosc=wielkosc;
+        this.opis=opis;
+    }
+
+    @Override
+    public String toString(){
+        return "Zwierze: [id="+id+",gatunek="+gatunek+",kolor ="+kolor+"wielkosc ="+wielkosc+"]" ;
+
+    }
+
+    public String getOpis(){return opis;}
+    public String getGatunek(){return gatunek;}
+    public String getKolor(){return kolor;}
+    public float getWielkosc(){return wielkosc;}
+    public int getId(){return id;}
+    public void setId(int id){this.id=id;}
+
+}
